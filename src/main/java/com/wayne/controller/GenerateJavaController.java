7a36cd9c4e30c90b4a2ec141cdcebdcab2362d8e
WayneZhao <wei_zhao2103@163.com>
2019-01-15 23:42:33 +0800

    抽取下载文件方法

--- src/main/java/com/wayne/controller/GenerateJavaController.java
+++ src/main/java/com/wayne/controller/GenerateJavaController.java
@@ -1,12 +1,13 @@
 package com.wayne.controller;
 
-import com.wayne.utils.StringUtils;
+import com.wayne.utils.FileUtils;
 import com.wayne.utils.ZipUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.*;
-import java.net.URLEncoder;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.zip.ZipOutputStream;
 
 @RestController
@@ -25,34 +26,8 @@ public class GenerateJavaController extends BaseController{
 
             ZipUtils.doZip(file,out,"");
 
-            InputStream inputStream = new FileInputStream(filePath+"GreetingController.zip");
+            FileUtils.downLoadFile(zipFileName,new FileInputStream(filePath+zipFileName),request,response);
 
-            BufferedOutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
-            URLEncoder.encode(fileName,"UTF-8");
-            response.setContentType("application/doc");
-            final String userAgent = request.getHeader("USER-AGENT");
-            if(StringUtils.contains(userAgent, "MSIE")){//IE浏览器
-                zipFileName = URLEncoder.encode(zipFileName,"UTF-8");
-            }else if(StringUtils.contains(userAgent, "Mozilla")){//google,火狐浏览器
-                zipFileName = new String(zipFileName.getBytes(), "ISO8859-1");
-            }else{
-                zipFileName = URLEncoder.encode(zipFileName,"UTF-8");//其他浏览器
-            }
-
-            response.addHeader("Content-Disposition","attachment;filename="+zipFileName);
-
-            byte[] car = new byte[1024];
-            int L;
-            while((L = inputStream.read(car)) != -1){
-                if( car.length != 0) {
-                    outputStream.write(car,0,L);
-                }
-            }
-
-            if(outputStream!= null) {
-                outputStream.flush();
-                outputStream.close();
-            }
 
         }catch (Exception e){
             e.printStackTrace();
---
+++ src/main/java/com/wayne/utils/FileUtils.java
@@ -0,0 +1,47 @@
+package com.wayne.utils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+
+public class FileUtils {
+    private FileUtils(){}
+
+    public static void downLoadFile(
+            String zipFileName,
+            FileInputStream fis,
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+
+        BufferedOutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+        URLEncoder.encode(zipFileName,"UTF-8");
+        response.setContentType("application/doc");
+        final String userAgent = request.getHeader("USER-AGENT");
+        if(StringUtils.contains(userAgent, "MSIE")){//IE浏览器
+            zipFileName = URLEncoder.encode(zipFileName,"UTF-8");
+        }else if(StringUtils.contains(userAgent, "Mozilla")){//google,火狐浏览器
+            zipFileName = new String(zipFileName.getBytes(), "ISO8859-1");
+        }else{
+            zipFileName = URLEncoder.encode(zipFileName,"UTF-8");//其他浏览器
+        }
+
+        response.addHeader("Content-Disposition","attachment;filename="+zipFileName);
+
+        byte[] car = new byte[1024];
+        int L;
+        while((L = fis.read(car)) != -1){
+            if( car.length != 0) {
+                outputStream.write(car,0,L);
+            }
+        }
+
+        if(outputStream!= null) {
+            outputStream.flush();
+            outputStream.close();
+        }
+    }
+}
